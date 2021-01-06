f979d07ef0c1c0796ffd87db2842c2765e4b55ec
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-06 16:46:41 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!bin/bash
 
 sudo apt install docker.io -y;
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
