239d2d30de27edf257112b1fa571fec1130b1826
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-06 18:48:39 +0900

    Update os.sh

--- os.sh
+++ os.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-sudo apt update -y && sudo apt upgrade && sudo apt install git -y;
+sudo apt update -y && sudo apt upgrade -y && sudo apt install git -y;
 
 sudo apt install zsh -y;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
