b1bae3e17b4eff3a8828b51ef2893383130d987e
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-06 16:46:19 +0900

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install docker.io -y;
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+
+docker-compose --version;
