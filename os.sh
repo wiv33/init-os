a57c27177945322e35778adbff727d91a87db477
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-06 16:45:44 +0900

    Update and rename init.sh to os.sh

--- os.sh
+++ os.sh
@@ -5,8 +5,4 @@ sudo apt update -y && sudo apt upgrade && sudo apt install git -y;
 sudo apt install zsh -y;
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
-sudo apt install docker.io -y;
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
-
-docker-compose --version;
 mkdir app
